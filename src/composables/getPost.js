e1b92a18c4a74118f61fcddd2be47f0c373077c9
James Bedford <jamesbedford@protonmail.com>
2021-12-31 01:22:12 +1100

    Added loading spinner componenent.

---
+++ src/composables/getPost.js
@@ -0,0 +1,29 @@
+import { ref } from "vue";
+
+const getPost = (id) => {
+    const post = ref(null)
+    const error = ref(null)
+
+    const load = async () => {
+      try {
+        // simulate delay
+        await new Promise(resolve => {
+            setTimeout(resolve, 1000)
+        })
+
+        let data = await fetch('http://localhost:3000/posts/' + id)
+        if (!data.ok) {
+          throw Error('That post does not exist.')
+        }
+        post.value = await data.json()
+      }
+      catch (err) {
+        error.value = err.message
+        console.log(error.value)
+      }
+    }
+
+    return { post, error, load }
+}
+
+export default getPost
--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -6,6 +6,11 @@ const getPosts = () => {
 
     const load = async () => {
       try {
+        // simulate delay
+        await new Promise(resolve => {
+          setTimeout(resolve, 1000)
+        })
+
         let data = await fetch('http://localhost:3000/posts')
         if (!data.ok) {
           throw Error('No data available.')
